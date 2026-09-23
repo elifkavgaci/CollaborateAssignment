8b48acd54cd5157070a4bc7ee7f0fb3a562138d5
Pelin Çınar <pelincinar007@gmail.com>
2026-09-23 15:25:36 +0300

    hallettim

--- App.java
+++ App.java
@@ -1,5 +1,18 @@
+
 public class App {
     public static void main(String[] args) {
-        
+        findSums(int[]arr){
+            int even = 0;
+            int odd = 0;
+            for(int i = 0; i < arr.length; i++){
+                if (i % 2 == 0) {
+                    even += arr[i];
+                }else{
+                    odd += arr[i];
+                }
+            }
+            System.out.println("Sum of even numbers: " + even);
+            System.out.println("Sum of odd numbers: " + odd);
+        }
     }
 }
