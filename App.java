fc2c0e875c6657b99f32b245bcf3033543c8013b
NehirHamaz <nehir.hamaz1@gmail.com>
2026-09-23 15:40:39 +0300

    find average and differences method

--- App.java
+++ App.java
@@ -1,5 +1,34 @@
 public class App {
+
+
+    public static void FindDiffArr(int[] arr){
+        //first find the average
+        int sum = 0;
+        int avg = 0;
+        for(int i = 0 ; i< arr.length;i++){
+            sum += arr[i];
+        }
+
+        avg = (sum / arr.length);
+
+        int[] differences = new int[arr.length];
+
+        for(int i = 0; i < differences.length; i++){
+            differences[i] = arr[i] - avg;
+        }
+
+        System.out.print("{");
+        for(int i = 0; i < differences.length; i++){
+            System.out.print(differences[i]);
+            if(i != differences.length-1){
+                System.out.print(",");
+            }
+        }
+        System.out.println("}");
+    }
+
+
     public static void main(String[] args) {
-        
+
     }
 }
